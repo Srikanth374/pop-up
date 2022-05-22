745d96d5f055d8246c41d86667f0fcd557da805c
Srikanth <vilasakavisrikanth@gmail.com>
2022-05-22 18:09:03 +0530

    initial commit

---
+++ src/components/Model.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './modal.css'
+
+function Model({props}) {
+  return ( 
+    <div className='ModalPopUp'>
+    <div className='modalContainer'>
+        <div className="titleCloseBtn">
+            <button onClick={() => props(false)}>x</button> 
+        </div>
+        <div className='title'>
+            <h1>Are Sure You Want to Continue</h1>
+        </div>
+        <div className='body'>
+            <h1>hello</h1>
+        </div>
+        <div className='footer'>
+            <button   onClick={() => props(false)} id="cancelBtn">Close</button>
+            <button>Continue</button>
+        </div>
+    </div>
+    </div>
+  )
+}
+export default Model
